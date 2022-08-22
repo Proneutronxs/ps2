5edd41a9fcef201669abc973b9ba30966cb12428
Josue <fumanny@hotmail.com>
2022-08-22 09:25:42 -0300

    cambio de url admin

--- ps/urls.py
+++ ps/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('administracion/de/recursos/de/about/y/project', admin.site.urls),
     path('', include('App.ps.urls')),
 ]
