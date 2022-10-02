ba8575248992a2f931c86944147383f3a173a916
Josue <fumanny@hotmail.com>
2022-10-02 15:42:57 -0300

    vistas2

--- App/business/urls.py
+++ App/business/urls.py
@@ -2,45 +2,53 @@ from django.urls import path
 from App.business import views
 from django.contrib.auth.views import LogoutView
 
-urlpatterns = [
-    
-    #### WEB
+from App.business.vistas.vistaSistemas import viewsSistemas
+from App.business.vistas.vistaEmpaque import viewsEmpaque
+from App.business.vistas.vistaAdministracion import viewsAdministracion
+from App.business.vistas.vistaCargas import viewsCargas
+from App.business.vistas.vistaCalidad import viewsCalidad
+from App.business.vistas.vistaDeposito import viewsDeposito
+from App.business.vistas.vistaChacras import viewsChacras
 
+    #### WEB
+urlpatterns = [
     path('', views.index, name="index"),
     path('zetone/', views.business, name="business"),
     
     
 
     ### ADMINISTRACIÓN
-    path('zetone/administracion/', views.administracion, name="administracion"),
+    path('zetone/administracion/', viewsAdministracion.administracion, name="administracion"),
 
     ### CALIDAD
-    path('zetone/calidad/', views.calidad, name="calidad"),
+    path('zetone/calidad/', viewsCalidad.calidad, name="calidad"),
 
     ### CARGAS
-    path('zetone/cargas/', views.cargas, name="cargas"),
+    path('zetone/cargas/', viewsCargas.cargas, name="cargas"),
 
     ### EMPAQUE
-    path('zetone/empaque/', views.empaque, name="empaque"),
+    path('zetone/empaque/', viewsEmpaque.empaque, name="empaque"),
 
 
     ### RONDIN
 
-    path('zetone/empaque/rondin/', views.rondin, name="rondin"),
-    path('zetone/empaque/rondin/newsereno', views.newSereno, name="newsereno"),
-    path('zetone/empaque/rondin/viewRecords', views.verRegistros, name="viewrecords"),
-    path('zetone/empaque/rondin/export', views.exportRondin, name="exportRondin"),
+    path('zetone/empaque/rondin/', viewsEmpaque.rondin, name="rondin"),
+    path('zetone/empaque/rondin/newsereno', viewsEmpaque.newSereno, name="newsereno"),
+    path('zetone/empaque/rondin/viewRecords', viewsEmpaque.verRegistros, name="viewrecords"),
+    path('zetone/empaque/rondin/export', viewsEmpaque.exportRondin, name="exportRondin"),
 
 
     ### DEPÓSITO
-    path('zetone/deposito/', views.deposito, name="deposito"),
+    path('zetone/deposito/', viewsDeposito.deposito, name="deposito"),
 
     ### CHACRAS
-    path('zetone/chacras/', views.chacras, name="chacras"),
+    path('zetone/chacras/', viewsChacras.chacras, name="chacras"),
 
     ### SISTEMAS
-    path('zetone/sistemas/', views.sistemas, name="sistemas"),
+    path('zetone/sistemas/', viewsSistemas.sistemas, name="sistemas"),
 
+    
+    
     ### API
 
     path('prueba', views.consulta, name="consulta"),
