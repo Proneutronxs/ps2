29c2eaf52b521d3335363f3b1f1eb963e8ac3e4e
Josue <fumanny@hotmail.com>
2022-10-17 14:54:39 -0300

    error2

--- ps/conexion.py
+++ ps/conexion.py
@@ -9,8 +9,8 @@ zt_psw = 'Sideswipe348'
 
 ###SERVIDOR PRONEUTRONXS
 ps_server = 'localhost'
-ps_user = 'Sideswipe'
-ps_psw = 'Sideswipe348'
+ps_user = 'psphpmyadminps'
+ps_psw = 'Proneutronxs$%Sideswipe$%1722'
 ps_port = '3306'
 
 
